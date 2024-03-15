7bd19685a90c32e7c8bfaadbc7fdbb3fd33a2076
rajat9926 <tonugupta9926@gmail.com>
2024-03-15 09:15:59 +0530

    new commit

--- bowlinggame.py
+++ bowlinggame.py
@@ -8,6 +8,9 @@ class Bowlinggame():
 		self.frames = 10
 		self.a = 0
 		self.round=1
+		self.trdball= 0
+		self.total = 0+self.trdball
+		self.allpinsdown = 0
 
 	def playername(self):
 		self.player1 = input("***Enter Your Name*** : ").capitalize()
@@ -17,24 +20,42 @@ class Bowlinggame():
 		permission = input(f"---- Round {self.round} ---- (Frames Left => {self.frames}) (y/n) : ")
 		if permission == "y":
 			self.frames-=1
-			t1p = input("Throw 1st Ball (y/n) : ")
-			t2p = input("Throw 2st Ball (y/n) : ")
-			ftr = random.randint(0,10) if t1p == "y" else exit()
+			# t1p = input("Throw 1st Ball (y/n) : ")
+			# t2p = input("Throw 2st Ball (y/n) : ")
+			ftr = random.randint(0,10) #if t1p == "y" else exit()
 			ft = 10-ftr
-			st = random.randint(0,ft) if t1p == "y" else exit()
-			total = ftr+st
+			st = random.randint(0,ft) #if t2p == "y" else exit()
+			self.total = ftr+st
 			self.round+=1
+
+			if self.round == 11:
+				self.trdball = random.randint(0,10)
+				self.score.append(self.score[self.a]+self.total+self.trdball)
+				self.a+=1
+				print(f"1st Throw - {ftr}|",f"2nd Throw - {st}|",f"3nd Throw - {self.trdball}", f"--> Total {self.total+self.trdball}")
+				self.checkscore()
+
+			if ftr == 10 or st == 10 or self.total == 10:
+				self.allpinsdown = 10
+
 			if len(self.score) == 0:
-				self.score.append(total)
-				print(f"1st Throw-{ftr}|",f"2nd Throw-{st}", f"-->total {total}")
+				self.score.append(self.total)
+				print(f"1st Throw-{ftr}|",f"2nd Throw-{st}", f"-->total {self.total}")
 				self.checkscore()
 			if len(self.score) >= 1:
-					if total == 0:
+					if self.total == 0:
 						self.score.append(0)
 					else:
-							self.score.append(self.score[self.a]+total)
-							self.a+=1
-			print(f"1st Throw - {ftr}|",f"2nd Throw - {st}", f"--> Total {total}")
+							# if self.allpinsdown == 10:
+							# 	print("  * * Congratulations Its A All Pin Down * *  ")
+							# 	tb = input("Throw 1st Ball (y/n) : ")
+							# 	ftrs = random.randint(0,10) if tb == "y" else exit()
+							# 	self.score.append(self.score[self.a]+10+ftrs)
+							# 	self.allpinsdown = 0
+							# else:
+								self.score.append(self.score[self.a]+self.total)
+								self.a+=1
+			print(f"1st Throw - {ftr}|",f"2nd Throw - {st}", f"--> Total {self.total}")
 			self.checkscore()
 		if permission == "n":
 			print(" ** Thank You Have A Good Day...Bye Bye ** ")
@@ -45,9 +66,9 @@ class Bowlinggame():
 
 	def checkscore(self):
 		if len(self.score) == 10:
-			self.finalscore = sum(self.score)
+			self.finalscore = self.score[self.a]
 			print(self.score)
-			print(f" Your Final Score Is =>  {self.finalscore} ")
+			print(f" Your Final Score Is =>  {self.finalscore} OUT OF 300")
 			print(" ** Thanks For Playing ** ")
 			exit()
 		else:
